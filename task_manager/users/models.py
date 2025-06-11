a28ae9cc6e4af5d71c0144d64f9fcfbb4b7a9937
Fedor.Shurgin <shurginfedor@gmail.com>
2025-06-11 21:49:44 +0300

    The third step

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -6,3 +6,6 @@ class UsersModels(models.Model):
     nickname = models.CharField(max_length=20)
     password = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "users_users"
