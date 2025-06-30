ebca8281a476a6e66d855870954669573e86596c
Fedor.Shurgin <shurginfedor@gmail.com>
2025-06-30 16:42:07 +0300

    The third step

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -4,20 +4,17 @@ from django.contrib.auth.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, label='Имя')
-    last_name = forms.CharField(max_length=30, label='Фамилия')
+    first_name = forms.CharField(label='Имя')
+    last_name = forms.CharField(label='Фамилия')
     username = forms.CharField(
-        max_length=30,
         label='Имя пользователя',
         help_text='Обязательное поле. Не более 150 символов. Только буквы, цифры и символы @/./+/-/_.'
         )
     password1 = forms.CharField(
-        max_length=30,
         label='Пароль',
         help_text='Ваш пароль должен содержать как минимум 3 символа.'
         )
     password2 = forms.CharField(
-        max_length=30,
         label='Подтверждение пароля',
         )
 
