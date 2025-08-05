0f542c0fc4303e0ac02ca599955b87c3dbaef863
Fedor.Shurgin <shurginfedor@gmail.com>
2025-08-05 14:57:04 +0300

    add messages

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -12,6 +12,10 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/create.html'
+    
+    def form_valid(self, form):
+        messages.success(self.request, "Пользователь успешно зарегистрирован!")
+        return super().form_valid(form)
 
 
 class UsersView(ListView):
