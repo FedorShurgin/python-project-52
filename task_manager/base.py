5cd562ffe223b985efe50cdece56c82a1e6c0647
Fedor.Shurgin <shurginfedor@gmail.com>
2025-08-05 21:44:54 +0300

    changed the statuses module

--- task_manager/base.py
+++ task_manager/base.py
@@ -1,8 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
 from django.contrib import messages
-
-from task_manager.statuses.models import StatusesModel
 
 
 class SuccessMessageMixin:
@@ -10,8 +6,3 @@ class SuccessMessageMixin:
     def form_valid(self, form):
         messages.success(self.request, self.success_message)
         return super().form_valid(form)
-
-
-class BaseStatusView(LoginRequiredMixin, SuccessMessageMixin):
-    model = StatusesModel
-    success_url = reverse_lazy('statuses:statuses')
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
-from django.contrib import messages
 
 
 from task_manager.statuses.forms import StatusesCreateForm
 from task_manager.statuses.models import StatusesModel
-from task_manager.base import BaseStatusView
+from task_manager.base import SuccessMessageMixin
+
+
+class BaseStatusView(LoginRequiredMixin, SuccessMessageMixin):
+    model = StatusesModel
+    success_url = reverse_lazy('statuses:statuses')
 
 
 class StatusesView(BaseStatusView, ListView):
@@ -31,34 +35,3 @@ class StatusesDeleteView(BaseStatusView, DeleteView):
     template_name = 'statuses/delete.html'
     context_object_name = 'status'
     success_message = "Статус успешно удалён"
-
-
-# class StatusesCreateView(LoginRequiredMixin, CreateView):
-#     model = StatusesModel
-#     form_class = StatusesCreateForm
-#     template_name = 'statuses/create.html'
-#     success_url = reverse_lazy('statuses:statuses')
-
-#     def form_valid(self, form):
-#         messages.success(self.request, "Статус успешно создан")
-#         return super().form_valid(form)
-
-
-# class StatusesUpdateView(LoginRequiredMixin, UpdateView):
-#     model = StatusesModel
-#     form_class = StatusesCreateForm
-#     template_name = 'statuses/update.html'
-#     context_object_name = 'status'
-#     success_url = reverse_lazy('statuses:statuses')
-
-#     def form_valid(self, form):
-#         messages.success(self.request, "Статус успешно изменен")
-#         return super().form_valid(form)
-
-
-# class StatusesDeleteView(LoginRequiredMixin, DeleteView):
-#     model = StatusesModel
-#     template_name = 'statuses/delete.html'
-#     context_object_name = 'status'
-#     success_url = reverse_lazy('statuses:statuses')
-  
