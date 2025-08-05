e3cf80018975938c7eb9ca36ab9fefb8d0a306da
Fedor.Shurgin <shurginfedor@gmail.com>
2025-08-06 00:48:34 +0300

    fix executor in task models

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -8,3 +8,10 @@ class TasksCreateForm(forms.ModelForm):
     class Meta:
         model = TasksModel
         fields = ['name', 'description', 'status', 'executor', 'labels']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['executor'].label_from_instance = lambda user: (
+            f"{user.first_name} {user.last_name}".strip() or user.username
+        )
