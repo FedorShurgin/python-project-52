4cba86e1927a285faf6b106dfcb482d0fed4c7c7
Fedor.Shurgin <shurginfedor@gmail.com>
2025-08-18 16:12:31 +0300

    add test user

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -5,6 +5,17 @@ from django.contrib.messages import get_messages
 from task_manager.users.forms import CustomUserCreationForm
 
 class SignUpViewTest(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        cls.valid_data = {
+            'username': 'NewUser',
+            'first_name': 'New_First_Name',
+            'last_name': 'New_last_Name',
+            'password1': 'test_password123',
+            'password2': 'test_password123',
+        }
+    
     def test_signup_view_uses_correct_template(self):
         resp = self.client.get(reverse('users:create'))
         self.assertTemplateUsed(resp, 'users/create.html')
@@ -18,26 +29,14 @@ class SignUpViewTest(TestCase):
     def test_successful_signup_redirects_to_login(self):
         resp = self.client.post(
             reverse('users:create'),
-            data={
-                'username': 'NewUser',
-                'first_name': 'New_First_Name',
-                'last_name': 'New_last_Name',
-                'password1': 'test_password123',
-                'password2': 'test_password123'
-            }
+            data=self.valid_data,
         )
         self.assertRedirects(resp, reverse('login'), status_code=302)
 
     def test_successful_signup_shows_message(self):
         resp = self.client.post(
             reverse('users:create'),
-            data={
-                'username': 'NewUser',
-                'first_name': 'New_First_Name',
-                'last_name': 'New_last_Name',
-                'password1': 'test_password123',
-                'password2': 'test_password123'
-            }
+            data=self.valid_data,
         )
         messages = list(get_messages(resp.wsgi_request))
         self.assertEqual(str(messages[0]), "Пользователь успешно зарегистрирован!")
@@ -46,12 +45,6 @@ class SignUpViewTest(TestCase):
         initial_count = User.objects.count()
         resp = self.client.post(
             reverse('users:create'),
-            data={
-                'username': 'NewUser',
-                'first_name': 'New_First_Name',
-                'last_name': 'New_last_Name',
-                'password1': 'test_password123',
-                'password2': 'test_password123'
-            }
+            data=self.valid_data,
         )
         self.assertEqual(User.objects.count(), initial_count + 1)
